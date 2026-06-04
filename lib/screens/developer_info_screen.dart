0669721a3e27031228e282d2f20207a0052fdf38
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-04 15:45:51 +0530

    feat: enhance developer info screen

--- lib/screens/developer_info_screen.dart
+++ lib/screens/developer_info_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class DeveloperInfoScreen extends StatelessWidget {
   const DeveloperInfoScreen({super.key});
